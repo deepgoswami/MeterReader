44f2c8834f0f191b251c1769b86419e69cf26b20
Deep Goswami <deepgo@gmail.com>
2013-06-17 02:08:19 +0530

    Saving bill data to the database in output file format added

---
+++ src/commgate/in/meterreader/OutputDataBean.java
@@ -0,0 +1,90 @@
+package commgate.in.meterreader;
+
+public class OutputDataBean 
+{
+	private String CONS_REF;
+	private String SDO_CD;
+	private String BINDER;
+	private String ACC_NO;
+	private String METERNO;
+	
+	private String MTRINSTL_DATE;
+	private String METERTYPE;
+	private String MF;
+	private String INITIAL_RED;
+	private String FINAL_RED_OLDMETER;
+	
+	private String OLDMETER_STAT;
+	private String CSTS_CD;
+	private String CUR_METER_STAT;
+	private String CURRRDG;
+	private String CUR_RED_DT;
+	
+	private String BILLING_DEMAND;
+	private String NEW_TRF_CD;
+	private String BILL_UNITS;
+	private String ENGCHG;
+	private String FIXCHG;
+	
+	private String METERRENT;
+	private String ED;
+	private String NEWBD;
+	private String NEWED;
+	private String NEWDPS;
+	
+	private String NEWOTH;
+	private String REFBLUNITS;
+	private String REFBLBD; 
+	private String REFBLED;
+	private String REFBLDPS;
+	
+	private String REFDEDUNITS;
+	private String REFDEDBD;
+	private String REFDEDED;
+	private String REFDEDDPS;
+	private String REB_OFF;
+	
+	private String NETBEFDUEDT;
+	private String NETAFTDUEDT;
+	private String BILLBASIS;
+	private String NOOFMONTHS;
+	private String REB_DT;
+	
+	private String ISSUE_DT;
+	private String BILLPERIOD;
+	private String BILLSERIALNO;
+	private String OLDCSTS_CD;
+	private String BILL_MTH;
+	
+	private String REMARKS;
+	private String MACHINE_SRL_NO;
+	private String MTR_READER_ID;
+	private String MTR_READER_NAME;
+	private String REB_OYT;
+	
+	private String REB_RTSWHT;
+	private String DOM_SPLREB;
+	private String ENGCHG_OLDTRF;
+	private String FIXCHG_OLDTRF;
+	private String ED_OLDTRF;
+	
+	private String NEWBD_OLDTRF;
+	private String NEWED_OLDTRF;
+	private String NEWDPS_OLDTRF;
+	private String NEWOTH_OLDTRF;
+	private String REFBLBD_OLDTRF;
+	
+	private String REFBLED_OLDTRF;
+	private String REFBLDPS_OLDTRF;
+	private String REFDEDBD_OLDTRF;
+	private String REFDEDED_OLDTRF;
+	private String REFDEDDPS_OLDTRF;
+	
+	private String REB_OFF_OLDTRF;
+	private String NETBEFDUEDT_OLDTRF;
+	private String NETAFTDUEDT_OLDTRF;
+	private String REB_HOSTEL;
+	private String MAX_DEMD;
+	
+	private String RECONN_CHARGE;
+}
